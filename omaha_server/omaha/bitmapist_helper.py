9a69818d40e2f949c0acf700d99cbeac71a5e2d8
Michael Herrmann <michael@herrmann.io>
2019-08-06 10:59:02 +0200

    Only use bitmapist-server to store statistics
    Originally, stats were stored in Redis. This was extremely memory
    intensive (30GB+). An alternative is bitmapist-server, which only needs
    a few hundred MB to store the same data. We've now run Redis and
    bitmapist-server in production in parallel for a month, with both
    backends being equally consistent and stable. So, now, we are disabling
    Redis (for stats - it's still used for other purposes) and are only
    using bitmapist-server instead. This allows us to decrease the server's
    RAM usage by 30+GB.

--- omaha_server/omaha/bitmapist_helper.py
+++ omaha_server/omaha/bitmapist_helper.py
@@ -1,44 +1,32 @@
 """
-The purpose of this module is to send Bitmapist data both to Redis (as is normal
-for Bitmapist) and to an instance of bitmapist-server. This allows us to
-evaluate the stability of the latter in our production use, to maybe, eventually
-use it to replace the RAM-hungry former.
+We use bitmapist to store usage statistics, such as the number of update
+requests per day. The default backend for bitmapist is Redis. However, due to
+the way bitmapist is implemented, this results in overwhelming RAM usage. To be
+precise, bitmapist uses bit strings to store information. Eg. "did user 13 make
+an update request on August 6" is stored as a binary 1 in the bit string for
+August 6.
+
+Not surprisingly, bitmapist's approach leads to huge RAM consumption. For
+example, for every new day there's a new bit string, stored in RAM, of length
+equal to the number of users. At the time of this writing, the RAM thus
+required is well over 30GB.
+
+To work around this, the creators of bitmapist created their own Redis
+implementation, bitmapist-server. It optimises the RAM usage of bit strings.
+Instead of tens of GB, it only needs a few hundred MB to store the same
+information.
+
+This module configures bitmapist to use bitmapist-server instead of Redis.
 """
-from io import BytesIO
-from traceback import print_exception
 
 from django.conf import settings
-from django.utils.timezone import localtime, now
-from django_redis import get_redis_connection
 from redis import Redis
 
 import bitmapist
-import sys
 
 def init():
-    global _REDIS_LOG_DB
-    _REDIS_LOG_DB = get_redis_connection('log')
-    master = Redis(
-        settings.REDIS_STAT_HOST, settings.REDIS_STAT_PORT,
-        settings.REDIS_STAT_DB, settings.REDIS_PASSWORD
-    )
-    slave = _BitmapistRedis(settings.BITMAPIST_HOST, settings.BITMAPIST_PORT)
-    bitmapist.SYSTEMS['default'] = _TeeRedis(master, slave)
-
-_REDIS_LOG_DB = None
-# Limit the number of log entries stored in Redis.
-# This prevents them from filling up all available memory:
-_NUM_REDIS_LOGS = 10000
-
-def _log(message):
-    # Log to Redis (not to a file on disk) for speed.
-    message = localtime(now()).isoformat() + ' ' + message
-    _REDIS_LOG_DB.set(_REDIS_LOG_DB.incr('i') % _NUM_REDIS_LOGS, message)
-
-def _log_curr_exception():
-    traceback = BytesIO()
-    print_exception(*sys.exc_info(), file=traceback)
-    _log(traceback.getvalue().decode('utf-8'))
+    bitmapist.SYSTEMS['default'] = \
+        _BitmapistRedis(settings.BITMAPIST_HOST, settings.BITMAPIST_PORT)
 
 class _BitmapistRedis(Redis):
     def flushdb(self):
@@ -47,55 +35,3 @@ class _BitmapistRedis(Redis):
         keys = self.keys()
         if keys:
             self.delete(*keys)
-
-class _Tee(object):
-    """
-    A proxy object that forwards all method calls to *two* other objects.
-    Optionally, it verifies that methods whose names are listed in CHECK return
-    the same result for both objects.
-    """
-    CHECK = set()
-    def __init__(self, master, slave):
-        self._master = master
-        self._slave = slave
-    def __getattr__(self, item):
-        result = getattr(self._master, item)
-        if callable(result):
-            check = item in self.CHECK
-            result = _TeeFn(result, getattr(self._slave, item), check)
-        return result
-
-class _TeeRedis(_Tee):
-    CHECK = ('bitcount', 'getbit', 'bitop')
-    def __init__(self, master, slave):
-        super(_TeeRedis, self).__init__(master, slave)
-    def pipeline(self):
-        master_pipeline = self._master.pipeline()
-        try:
-            slave_pipeline = self._slave.pipeline()
-        except Exception:
-            _log_curr_exception()
-            return master_pipeline
-        return _Tee(master_pipeline, slave_pipeline)
-
-class _TeeFn:
-    def __init__(self, master, slave, check=False):
-        self._master = master
-        self._slave = slave
-        self._check = check
-    def __call__(self, *args, **kwargs):
-        master_result = self._master(*args, **kwargs)
-        try:
-            slave_result = self._slave(*args, **kwargs)
-        except Exception:
-            _log_curr_exception()
-            return master_result
-        if self._check and master_result != slave_result:
-            fn_name = self._master.__name__
-            args_repr = ', '.join(map(repr, args))
-            args_repr += ', '.join('%s=%r' % kwarg for kwarg in kwargs.items())
-            _log(
-                'Warning: %s(%s) returned %r instead of %r.'
-                % (fn_name, args_repr, slave_result, master_result)
-            )
-        return master_result
